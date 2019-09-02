d03ff6f7321ab7c5cfa9bb41af97b2da74eb3600
Concurrent <C.Y.Concurrent@gmail.com>
2019-09-02 21:00:57 +0800

    自动安装docker

--- install.sh
+++ install.sh
@@ -1,6 +1,20 @@
-docker build -t arrebole/Culaccino .
-docker network create net
+if ! [ -x "$(command -v docker)" ]; then
+    echo "install docker ..."
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable edge test"
+    #apt-cache policy docker-ce
+fi
+apt-get update
+# 更新docker
+apt-get install -y docker-ce
+# 删除全部容器
+docker rm -f $(docker ps -aq)
+# 创建docker网络
+docker network create futanaicha
+# 构建镜像
+docker build -t arrebole/Culaccino:v0.8 .
 mkdir -p $(pwd)/wwwroot/static
+# 启动容器
 docker run \
 --net net \
 --name culaccino \
