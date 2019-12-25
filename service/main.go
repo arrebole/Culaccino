17e9ce1aa4a527c46e19e6bd8db31f4f747ccf0d
concurrent <concurrent.exec@gmail.com>
2019-12-25 15:13:58 +0800

    add static file

--- service/main.go
+++ service/main.go
@@ -11,5 +11,5 @@ func main() {
 	http.HandleFunc("/api/directory", controllers.DirectoryCtl(config.DirectoryfilePath))
 	http.HandleFunc("/api/source", controllers.SourceCtl(config.SourceRoot))
 	http.HandleFunc("/", controllers.FileServer(config.StaticRoot))
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe("127.0.0.1:3000", nil)
 }
---
+++ themes/build/precache-manifest.398debe7ba7f89d6e4edb5216b11be8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0f178d2a1b92668842bc198d86f7a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "948ca3d764dbbaba043b",
+    "url": "/static/css/2.a69888c1.chunk.css"
+  },
+  {
+    "revision": "75f6b0f0618cad356626",
+    "url": "/static/css/main.1231a5ef.chunk.css"
+  },
+  {
+    "revision": "948ca3d764dbbaba043b",
+    "url": "/static/js/2.0da8e521.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.0da8e521.chunk.js.LICENSE"
+  },
+  {
+    "revision": "75f6b0f0618cad356626",
+    "url": "/static/js/main.57db7332.chunk.js"
+  },
+  {
+    "revision": "c95e005b84d0ca07ad31",
+    "url": "/static/js/runtime-main.9f659dd1.js"
+  }
+]);
